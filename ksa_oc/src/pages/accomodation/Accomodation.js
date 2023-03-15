c8d71cf4529b37b041ed15f17bd64fdb833b5a96
Corentin "Codch" Petit <corentin.cpetit@gmail.com>
2023-03-15 17:07:29 +0100

    Fin page accomodation

--- ksa_oc/src/pages/accomodation/Accomodation.js
+++ ksa_oc/src/pages/accomodation/Accomodation.js
@@ -1,7 +1,9 @@
 import './accomodation.scss'
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
-// import Collapse from '../../components/collapse/Collapse'
+import Collapse from '../../components/collapse/Collapse'
+import greyStar from '../../assets/grey_star.png'
+import redStar from '../../assets/red_star.png'
 import Carousel from '../../components/carousel/Carousel'
 import { datas } from '../../data/data'
 import { useParams } from 'react-router-dom'
@@ -21,10 +23,61 @@ export default function Accomodation() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [idAccomodation])
 
+  const name = dataCurrentAccomodation[0].host.name.split(' ')
+  const rating = dataCurrentAccomodation[0].rating
+  const description = dataCurrentAccomodation[0].description
+  const equipments = dataCurrentAccomodation[0].equipments
+
   return (
     <div>
       <Header />
       <Carousel imageSlider={imageSlider} />
+      <main className="accomodation">
+        <div className="accomodation_content">
+          <div className="accomodation_content_infos">
+            <h1>{dataCurrentAccomodation[0].title}</h1>
+            <p>{dataCurrentAccomodation[0].location}</p>
+            <div>
+              {dataCurrentAccomodation[0].tags.map((tag, index) => {
+                return <button key={index}>{tag}</button>
+              })}
+            </div>
+          </div>
+          <div className="accomodation_content_host">
+            <div>
+              <div className="accomodation_content_host_name">
+                <span>{name[0]}</span>
+                <span>{name[1]}</span>
+              </div>
+              <img
+                src={dataCurrentAccomodation[0].host.picture}
+                alt="host of this accomodation"
+              />
+            </div>
+
+            <div className="accomodation_content_host_stars">
+              {[...Array(5)].map((star, index) => {
+                const ratingValue = index + 1
+                return (
+                  <img
+                    key={index}
+                    src={ratingValue <= rating ? redStar : greyStar}
+                    alt="star"
+                  />
+                )
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="accomodation_collapse">
+          <div className="accomodation_collapse_item">
+            <Collapse title={'Description'} content={description} />
+          </div>
+          <div className="accomodation_collapse_item">
+            <Collapse title={'Équipements'} content={equipments} />
+          </div>
+        </div>
+      </main>
       <Footer />
     </div>
   )
